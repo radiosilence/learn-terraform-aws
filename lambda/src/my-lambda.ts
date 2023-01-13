b859a9742038613002072dab390d98274d5d9526
James Cleveland <james.cleveland@engineerbetter.com>
2023-01-13 10:57:18 +0000

    make function typed and return something normal

--- lambda/src/my-lambda.ts
+++ lambda/src/my-lambda.ts
@@ -1,4 +1,12 @@
-export function handler(blah?: number) {
-  console.log("i have run", blah, process.env.MY_TABLE_NAME);
-  return "hi";
-}
+import { APIGatewayProxyHandler } from "aws-lambda";
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  console.log("i have run", event, context, process.env.MY_TABLE_NAME);
+  return {
+    statusCode: 200,
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ message: "hello world", event, context }),
+  };
+};
