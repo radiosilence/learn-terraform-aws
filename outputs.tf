aac386e8b0c0479a0968875a354f5a8cf973cae6
James Cleveland <james.cleveland@engineerbetter.com>
2023-01-13 12:20:36 +0000

    more fun outputs

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,13 @@ output "base_url" {
   description = "The base URL of the API Gateway."
   value       = aws_apigatewayv2_stage.lambda.invoke_url
 }
+
+output "hello_url" {
+  description = "The URL of the hello route."
+  value       = "${aws_apigatewayv2_stage.lambda.invoke_url}/hello"
+}
+
+output "hello_person_url" {
+  description = "The URL of the hello/{name} route."
+  value       = "${aws_apigatewayv2_stage.lambda.invoke_url}/hello/baz"
+}
