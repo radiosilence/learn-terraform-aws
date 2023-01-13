f38eebefb5f8aa656a5401efae94e40fc28bdf5f
James Cleveland <james.cleveland@engineerbetter.com>
2023-01-13 11:50:20 +0000

    yesss

--- lambda/src/demo/hello.ts
+++ lambda/src/demo/hello.ts
@@ -1,12 +1,16 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
-  console.log("i have run", event, context, process.env.MY_TABLE_NAME);
+  const debug = { event, context, env: process.env };
+  console.log("i have run", debug);
   return {
     statusCode: 200,
     headers: {
       "content-type": "application/json",
     },
-    body: JSON.stringify({ message: "hello world", event, context }),
+    body: JSON.stringify({
+      message: "hello world",
+      debug,
+    }),
   };
 };
