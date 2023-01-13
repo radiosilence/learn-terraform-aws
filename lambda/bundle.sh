f4ba7f278f38fda15f24eaa9b70ab055a7f03728
James Cleveland <james.cleveland@engineerbetter.com>
2023-01-13 16:46:10 +0000

    some comment

--- lambda/bundle.sh
+++ lambda/bundle.sh
@@ -12,12 +12,14 @@ bundle() {
     package_file="$build_dir/package.zip"
     mkdir -p "$build_dir"
     echo "Compiling $name:  $1 -> $bundle_file -> $package_file..."
+    # esbuild is the recommended way to bundle JS now and is fast/awesome
     yarn esbuild \
         --bundle "$f" \
         --target=node18 \
         --format=esm \
         --external:"@aws-sdk/*" \
         --outfile="$bundle_file"
+    # We set the date to a fixed date so that the zip file is always the same if contents are same
     find "$build_dir" -exec touch -t 198909022153 {} +
     (zip -jX "$package_file" "$bundle_file")
 }
