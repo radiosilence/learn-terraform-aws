9522378b9196400dc8e46ca6e6470c2bdaeb56e7
James Cleveland <james.cleveland@engineerbetter.com>
2023-01-13 12:03:14 +0000

    use default stage

--- gateway.tf
+++ gateway.tf
@@ -6,7 +6,7 @@ resource "aws_apigatewayv2_api" "lambda" {
 resource "aws_apigatewayv2_stage" "lambda" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  name        = var.env
+  name        = "$default"
   auto_deploy = true
 
   access_log_settings {
