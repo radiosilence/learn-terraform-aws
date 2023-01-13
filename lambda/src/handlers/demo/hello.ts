09e3ece77b04501325a4ec08a2b90908ccb6847d
James Cleveland <james.cleveland@engineerbetter.com>
2023-01-13 13:16:27 +0000

    more comments

--- lambda/src/handlers/demo/hello.ts
+++ lambda/src/handlers/demo/hello.ts
@@ -4,18 +4,24 @@ import { ddbDocClient } from "../../libs/ddbDocClient";
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
   const debug = { event, context, env: process.env };
-  console.log("i have run", debug);
+  console.log("Running handler", debug);
 
   const TableName = process.env.TABLE_potato;
   if (!TableName) {
     throw new Error("TABLE_potato is not defined");
   }
 
+  console.log("TableName", TableName);
+
   const name = event.pathParameters?.name ?? "world";
+
+  console.log("name", name);
+  console.log("Sending GetCommand...");
   const data = await ddbDocClient.send(
     new GetCommand({ TableName, Key: { primaryKey: name } })
   );
 
+  console.log("Got data", data);
   const Item = data?.Item ?? { id: name, count: 1 };
 
   // NOTE: Not atomic/concurrency safe in any way
@@ -23,6 +29,7 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
     Item.count += data.Item.count;
   }
 
+  console.log("Sending PutCommand...");
   await ddbDocClient.send(new PutCommand({ TableName, Item }));
 
   return {
